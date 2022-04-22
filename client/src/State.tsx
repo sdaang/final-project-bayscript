67e2db05fe016cabb4ac89bd045ca174f9ce73aa
sdaang <sdaang96@gmail.com>
2022-04-21 21:10:34 -0700

    updated visualizer

--- client/src/State.tsx
+++ client/src/State.tsx
@@ -3,7 +3,12 @@ import { List, Map } from 'immutable';
 
 // project dependencies
 import { PianoInstrument } from './instruments/Piano';
+import { PianoTest } from './instruments/PianoTest';
+// import { FluteInstrument } from './instruments/Flute';
+
+
 import { WaveformVisualizer } from './visualizers/Waveform';
+import { WaveformVisualizerFlute } from './visualizers/Flute_Waveform';
 
 
 /** ------------------------------------------------------------------------ **
@@ -15,14 +20,13 @@ export type AppState = Map<string, any>;           // similar to { [id: string]:
  * Start with the default piano instrument.
  * Add your instruments to this list.
  */
-const instruments = List([PianoInstrument]);       // similar to Instrument[]
+const instruments = List([PianoInstrument, PianoTest]);     // similar to Instrument[]
 
 /**
  * Start with the default waveform visualizer.
  * Add your visualizers to this list.
  */
-const visualizers = List([WaveformVisualizer]);    // similar to Visualizer[]
-
+const visualizers = List([WaveformVisualizer, WaveformVisualizerFlute]);    // similar to Visualizer[]
 
 /**
  * The default application state contains a list of instruments and a list of visualizers.
