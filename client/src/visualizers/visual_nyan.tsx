773f117af8a55a74ceab675a0c4d236a4c49418a
nyanjustint <nyanjustint@gmail.com>
2022-05-01 22:19:17 -0700

    wrapped ellipse visualizer

--- client/src/visualizers/visual_nyan.tsx
+++ client/src/visualizers/visual_nyan.tsx
@@ -6,13 +6,13 @@ import * as Tone from 'tone';
 import { Visualizer } from '../Visualizers';
 
 //test
-export const FlowerVisualizer = new Visualizer(
-    'FlowerVisualizer (Nyan)', (p5: P5, analyzer: Tone.Analyser) => {
-        const width = 50;
+export const EllipseVisual = new Visualizer(
+    'EllipseVisual', (p5: P5, analyzer: Tone.Analyser) => {
+        const width = 300;
         const height = 100;
         const colors = ['purple', 'green', 'yellow', 'red']
         const strokeWeight = colors[Math.floor(Math.random() * (colors.length))];
-
+        p5.fill(strokeWeight);
         p5.background('black');
         p5.stroke(String(strokeWeight));
         const values = analyzer.getValue();
@@ -20,21 +20,21 @@ export const FlowerVisualizer = new Visualizer(
         p5.beginShape();
 
         for(let i=0; i<values.length; i++){
-            const amplitude = values[i] as number;
-            const x = p5.map(i, 0, values.length-1, 0, width/2);
-            const y = p5.map(i, 0, values.length-1, 0, height/2);
-            const angleX = p5.map(i, 0, x, Math.PI*(-4), 4*Math.PI);
-            const angleY = p5.map(i, 0, y, Math.PI*(-4), 4*Math.PI);
-            const angle = angleX * (x/width) + angleY * (y/height);
-
-            for(let j=0; j<10; j++) {
-                const drawX = (width*(-4)) - width / 2 * p5.cos(2 * Math.PI * amplitude + angle);
-                const drawY = (height*(-0.5)) - height / 2 * p5.sin(2 * Math.PI * -1 * amplitude + angle);
-                p5.circle(drawX + j * 145, drawY, 10);
-                p5.circle(drawX + j * 145, drawY + 1 * 150, 10);
-                p5.circle(drawX + j * 145, drawY, 10);
-                p5.circle(drawX + j * 145, drawY + 1 * 150, 10);
-            }
+            const index = p5.map(i, 0, 80, 0, values.length+1);
+            const radius = p5.map(values[index] as number, -1, 1, 100, 300);
+            const x = radius * Math.sin(i);
+            const y = radius * Math.cos(i);
+            
+            const place = [80,180];
+            const randomX = place[Math.floor(Math.random() * (place.length))]
+            p5.ellipse(x+200,y,randomX,randomX);
+            p5.ellipse(x+400,y,randomX,randomX);
+            p5.ellipse(x+500,y-200,randomX,randomX);
+            p5.ellipse(x+600,y+100,randomX,randomX);
+            p5.ellipse(x+700,y-150,randomX,randomX);
+            p5.ellipse(x+800,y,randomX,randomX);
+            p5.ellipse(x+1000,y-100,randomX,randomX);
+            
         }
         p5.endShape();
     },
