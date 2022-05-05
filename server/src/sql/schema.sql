03df803c3a8396815597d707b6ce878441f96ff8
sdaang <sdaang96@gmail.com>
2022-05-05 05:56:13 -0700

    Added 1 song to test

--- server/src/sql/schema.sql
+++ server/src/sql/schema.sql
@@ -1,8 +1,26 @@
 CREATE TABLE songs (
 	id int NOT NULL PRIMARY KEY,
 	song_title text NOT NULL,
+	artist text NOT NULL,
 	notes varchar NOT NULL
 );
 
-INSERT INTO songs (id, song_title, notes) 
-VALUES (1, 'Ode to Joy (Dubstep Remix)', 'E4 E4 F4 G4 G4 F4 E4 D4 C4 C4 D4 E4 E4 D4 D4');
+
+INSERT INTO songs (id, song_title, artist, notes) 
+VALUES (1, 'Ode to Joy (Dubstep Remix)', 'Ludwig van Beethoven','E4 E4 F4 G4 G4 F4 E4 D4 C4 C4 D4 E4 E4 D4 D4');
+
+INSERT INTO songs (id, song_title, artist, notes) 
+
+VALUES (2, 'Giornos Theme', 'Yugo Kanno', 'Gb4 Gb4 Gb4 
+D4 D4 D4 D4 D4
+D4 E4 
+F4 F4 E4 E4 D4 
+Db4 Db4 D4 D4 E4
+Gb4 Gb4 Gb4 Gb4
+B4 B4 B4 B4
+B3 Db4 
+D4 D4 E4 E4 D4 
+Db Db A4 A4 G4
+F5 F5 F5 D5 D5 D5 D5 D5 E5');
+
+
