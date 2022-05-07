922e51998f79cbce173ad28557a31bd905a6b6af
sdaang <sdaang96@gmail.com>
2022-05-07 05:07:53 -0700

    Updated SideNav.tsx
    CSS applied to song metadata and added button for future modal window

--- client/src/SideNav.tsx
+++ client/src/SideNav.tsx
@@ -1,20 +1,19 @@
 // 3rd party library imports
-import classNames from 'classnames';
-import { List } from 'immutable';
-import React from 'react';
-import { useLocation, Link } from 'react-router-dom';
+import classNames from "classnames";
+import { List } from "immutable";
+import React, {useState, useEffect} from "react";
+import { useLocation, Link } from "react-router-dom";
 import {
   RadioButton20,
   RadioButtonChecked20,
   Music20,
-} from '@carbon/icons-react';
+} from "@carbon/icons-react";
 
 // project imports
-import { DispatchAction } from './Reducer';
-import { AppState } from './State';
-import { Instrument } from './Instruments';
-import { Visualizer } from './Visualizers';
-
+import { DispatchAction } from "./Reducer";
+import { AppState } from "./State";
+import { Instrument } from "./Instruments";
+import { Visualizer } from "./Visualizers";
 
 /** ------------------------------------------------------------------------ **
  * SideNav component
@@ -27,7 +26,7 @@ type SideNavProps = {
 
 export function SideNav({ state, dispatch }: SideNavProps): JSX.Element {
   /**
-   * 
+   *
    * SideNav
    * |-----------------|
    * | Nameless App    |
@@ -38,7 +37,7 @@ export function SideNav({ state, dispatch }: SideNavProps): JSX.Element {
    * | InstrumentsNav  |
    * | |-----------|   |
    * | |           |   |
-   * | |-----------|   | 
+   * | |-----------|   |
    * |                 |
    * | VisualizersNav  |
    * | |-----------|   |
@@ -51,7 +50,7 @@ export function SideNav({ state, dispatch }: SideNavProps): JSX.Element {
    * | |-----------|   |
    * |                 |
    * |-----------------|
-  */
+   */
 
   return (
     <div className="absolute top-0 left-0 bottom-0 w5 z-1 shadow-1 bg-white flex flex-column">
@@ -62,37 +61,37 @@ export function SideNav({ state, dispatch }: SideNavProps): JSX.Element {
         <InstrumentsNav state={state} dispatch={dispatch} />
         <VisualizersNav state={state} dispatch={dispatch} />
         <SongsNav state={state} dispatch={dispatch} />
+        <SongsModalWindow state={state} dispatch={dispatch} />
       </div>
     </div>
   );
 }
 
-
 /** ------------------------------------------------------------------------ **
  * SideNav Sub-Components
  ** ------------------------------------------------------------------------ */
 
- function InstrumentsNav({ state }: SideNavProps): JSX.Element {
-  /** 
+function InstrumentsNav({ state }: SideNavProps): JSX.Element {
+  /**
    *  InstrumentsNav
    *  |-----------------|
    *  | Section         |
    *  | |-------------| |
    *  | | RadioButton | |
-   *  | |-------------| | 
+   *  | |-------------| |
    *  | | RadioButton | |
    *  | |-------------| |
    *  |      ...        |
    *  |-----------------|
-  */
-  
-  const instruments: List<Instrument> = state.get('instruments');
-  const activeInstrument = state.get('instrument')?.name;
+   */
+
+  const instruments: List<Instrument> = state.get("instruments");
+  const activeInstrument = state.get("instrument")?.name;
   const location = useLocation();
 
   return (
     <Section title="Instruments">
-      {instruments.map(i => (
+      {instruments.map((i) => (
         <RadioButton
           key={i.name}
           to={`/${i.name}${location.search}`}
@@ -106,26 +105,26 @@ export function SideNav({ state, dispatch }: SideNavProps): JSX.Element {
 }
 
 function VisualizersNav({ state }: SideNavProps): JSX.Element {
-  /** 
+  /**
    *  VisualizersNav
    *  |-----------------|
    *  | Section         |
    *  | |-------------| |
    *  | | RadioButton | |
-   *  | |-------------| | 
+   *  | |-------------| |
    *  | | RadioButton | |
    *  | |-------------| |
    *  |      ...        |
    *  |-----------------|
-  */
+   */
 
-  const visualizers: List<Visualizer> = state.get('visualizers');
-  const activeVisualizer = state.get('visualizer')?.name;
+  const visualizers: List<Visualizer> = state.get("visualizers");
+  const activeVisualizer = state.get("visualizer")?.name;
   const location = useLocation();
 
   return (
     <Section title="Visualizers">
-      {visualizers.map(v => (
+      {visualizers.map((v) => (
         <RadioButton
           key={v.name}
           to={{
@@ -142,39 +141,62 @@ function VisualizersNav({ state }: SideNavProps): JSX.Element {
 }
 
 function SongsNav({ state, dispatch }: SideNavProps): JSX.Element {
-  /** 
-   * 
+  /**
+   *
    *  SongsNav
    *  |-----------------|
    *  | Section         |
    *  | |-------------| |
    *  | | Music20     | |
-   *  | |-------------| | 
+   *  | |-------------| |
    *  | | Music20     | |
    *  | |-------------| |
    *  |      ...        |
    *  |-----------------|
-  */
+   */
 
-  const songs: List<any> = state.get('songs', List());
+  const songs: List<any> = state.get("songs", List());
   return (
     <Section title="Playlist">
-      {songs.map(song => (
+      {songs.map((song) => (
         <div
-          key={song.get('id')}
-          className="f6 pointer underline flex items-center no-underline i dim"
+          key={song.get("id")}
+          className="f6 pointer underline items-center no-underline i dim"
           onClick={() =>
-            dispatch(new DispatchAction('PLAY_SONG', { id: song.get('id') }))
+            dispatch(new DispatchAction("PLAY_SONG", { id: song.get("id") }))
           }
         >
           <Music20 className="mr1" />
-          {song.get('songTitle')}
+          {song.get("songTitle")}
+
+          <div className="metaDataPlacement">{song.get("artist")}</div>
         </div>
       ))}
     </Section>
   );
 }
 
+
+/** ------------------------------------- **
+ * Song Dashboard
+ ** ------------------------------------- */
+
+function SongsModalWindow({ state, dispatch }: SideNavProps): JSX.Element {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  return (
+    <>
+    <button type="button" className="button" onClick={handleShow}>
+      Playlist Dashboard
+    </button>
+{/* 
+    <div className="modalWindow" show={show} onHide></div> */}
+
+   </>
+  );
+}
 
 /** ------------------------------------------------------------------------ **
  * Auxilliary components
@@ -184,18 +206,23 @@ function SongsNav({ state, dispatch }: SideNavProps): JSX.Element {
  * Radio Button
  ** ------------------------------------- */
 
- type RadioButtonProps = {
-  to: any,
-  text: string,
-  active: boolean,
-  onClick: () => void
+type RadioButtonProps = {
+  to: any;
+  text: string;
+  active: boolean;
+  onClick: () => void;
 };
 
-function RadioButton({ to, text, active, onClick }: RadioButtonProps): JSX.Element {
+function RadioButton({
+  to,
+  text,
+  active,
+  onClick,
+}: RadioButtonProps): JSX.Element {
   return (
     <Link to={to} className="no-underline">
       <div
-        className={classNames('f6 flex items-center black', { fw7: active })}
+        className={classNames("f6 flex items-center black", { fw7: active })}
         onClick={onClick}
       >
         {active ? (
@@ -208,7 +235,6 @@ function RadioButton({ to, text, active, onClick }: RadioButtonProps): JSX.Eleme
     </Link>
   );
 }
-
 
 /** ------------------------------------- **
  * Section
