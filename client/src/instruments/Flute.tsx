fbfefd361b6914011585ebb80a3d23522728380a
sdaang <sdaang96@gmail.com>
2022-05-07 05:05:39 -0700

    Updated Flute.tsx

--- client/src/instruments/Flute.tsx
+++ client/src/instruments/Flute.tsx
@@ -6,8 +6,6 @@ import React, { useEffect } from "react";
 
 // project imports
 import { Instrument, InstrumentProps } from "../Instruments";
-import { RecursivePartial } from "tone/Tone/core/util/Interface";
-import { OmniOscillatorOptions } from "tone/Tone/source/oscillator/OscillatorInterface";
 
 /** ------------------------------------------------------------------------ **
  * References:
@@ -44,18 +42,25 @@ export function FluteKey({
     // 3. The curly braces `{` and `}` should remind you of string interpolation.
     <div
       onMouseDown={() => synth?.triggerAttack(`${note}`)} // Question: what is `onMouseDown`?
-      onMouseUp={() => synth?.triggerRelease("+0.25")} // Question: what is `onMouseUp`?
+      onMouseUp={() => synth?.triggerRelease("+0.30")} // Question: what is `onMouseUp`?
       className={classNames("ba pointer absolute dim", {
-        "bg-black black h3": minor, // minor keys are black
-        "black bg-white h4": !minor, // major keys are white
+        'black bg-medium-gray h4': note,
       })}
       style={{
-        // CSS
+        // top: 0,
+        // left: `${index * 2}rem`,
+        // zIndex: minor ? 1 : 0,
+        // width: minor ? '1.5rem' : '2rem',
+        // marginLeft: minor ? '0.25rem' : 0,
+
         top: 0,
-        left: `${index * 2}rem`,
-        zIndex: minor ? 1 : 0,
-        width: minor ? "1.5rem" : "2rem",
-        marginLeft: minor ? "0.25rem" : 0,
+        left: `${index * 5}rem`,
+        zIndex: 0,
+        width: '2rem',
+        marginLeft: 25,
+        height: 30,
+        borderRadius: 50,
+        backgroundColor: "black",
       }}
     ></div>
   );
@@ -75,32 +80,47 @@ function FluteKeyWithoutJSX({
   return React.createElement(
     "div",
     {
-      onMouseDown: () => synth?.triggerAttack(`${note}`),
-      onMouseUp: () => synth?.triggerRelease("+0.25"),
-      className: classNames("ba pointer absolute dim", {
-        "bg-black black h3": minor,
-        "black bg-white h4": !minor,
+      onMouseDown: () => synth?.triggerAttack(`${note}`, "8n"),
+      onMouseUp: () => synth?.triggerRelease("+0.30"),
+      className: classNames("ba pointer absolute dim flute", {
+        "black bg-medium-gray h4": note,
       }),
       style: {
+        // top: 0,
+        // left: `${index * 2}rem`,
+        // zIndex: minor ? 1 : 0,
+        // width: minor ? "1.5rem" : "2rem",
+        // marginLeft: minor ? "0.25rem" : 0,
+     
         top: 0,
-        left: `${index * 2}rem`,
-        zIndex: minor ? 1 : 0,
-        width: minor ? "1.5rem" : "2rem",
-        marginLeft: minor ? "0.25rem" : 0,
+        left: `${index * 5}rem`,
+        zIndex: 0,
+        width: '2rem',
+        marginLeft: 3,
+        height: 30,
+        borderRadius: 50,
+        backgroundColor: "black",
+        
       },
     },
     []
   );
 }
 
+
+// Flute type -> 
 function PianoType({ title, onClick, active }: any): JSX.Element {
   return (
     <div
       onClick={onClick}
-      className={classNames("dim pointer ph2 pv1 ba mr2 br1 fw7 bw1", {
-        "b--black black": active,
-        "gray b--light-gray": !active,
-      })}
+      className={classNames(
+        "dim pointer ph2 pv1 ba mr2 br1 fw7 fluteOscillatorBtns",
+        {
+          //CSS for buttons
+          "b--black black ": active,
+          "gray b--light-gray": !active,
+        }
+      )}
     >
       {title}
     </div>
@@ -127,48 +147,49 @@ function Flute({ synth, setSynth }: InstrumentProps): JSX.Element {
     setSynth((oldSynth) => {
       oldSynth.disconnect();
       return new Tone.Synth({
-        volume: -6,
+        volume: 4,
         portamento: 0,
         detune: 0,
         envelope: {
-          attack: 5,
+          attack: 5,  //The shape of the attack (not sin, starts off too quiet.)
           attackCurve: "cosine",
           decay: 1,
-          decayCurve: "exponential", //The shape of the decay either "linear" or "exponential"
+          decayCurve: "exponential", //The shape of the decay "exponential" like a flute (air cut off)
           release: 5,
-          releaseCurve: "exponential",
+          releaseCurve: "exponential", 
           sustain: 0,
         },
         oscillator: {
-          partialCount: 4,
-          partials: [0.000000007, 0.00007777, 0.0077, 1],
-          phase: 3,
           type: "fatcustom",
+          // The first harmonic is the fundamental frequency, the second is the octave 
+          // and so on following the harmonic series.
+          // 1, 0.2, 0.01 Sounds like Bass Clarinet (smaller the number the higher it sounds)
+          // Need to go up for flute
+          partials: [0.000000007, 0.00007777, 0.0077, 1.5],
+          phase: 3,
           /*The detune spread between the oscillators. If "count" is set to 3 oscillators and the "spread" 
             is set to 40, the three oscillators would be detuned like this: [-20, 0, 20] for a total detune spread 
             of 40 cents.*/
           spread: 1,
-        } as RecursivePartial<OmniOscillatorOptions>,
+        },
       }).toDestination();
     });
   };
 
   const oscillators: List<OscillatorType> = List([
-    "fmsine",
-    "sawtooth",
-    "square",
-    "triangle",
-    "sine",
-    "fmsawtooth",
-    "fmtriangle",
-    "amsine",
-    "amsawtooth",
-    "amtriangle",
+    'sine',
+    'sine10',
+    'amsine10',
+    'fmsawtooth3',
+    'sawtooth',
+    'square',
+    'triangle',
+    'amtriangle10',
   ]) as List<OscillatorType>;
 
   return (
-    <div className="pv4">
-      <div className="relative dib h4 w-100 ml4">
+    <div className="pv4" id="flute">
+      <div className="relative h4 w-100 ml4">
         {Range(2, 5).map(
           (
             octave // Flute only has 3 octaves
@@ -199,8 +220,10 @@ function Flute({ synth, setSynth }: InstrumentProps): JSX.Element {
           />
         ))}
       </div>
-      <h2>Flute Representation</h2>
-      <div className="flute"><p>"sdfsdfs"</p></div>
+      {/* <div className="pv6" id="flute">
+
+      
+      </div> */}
     </div>
   );
 }
